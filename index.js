1a414ced2ba423c5ff522cfacb3af48fb50cd774
Brian Kaloki <brianngenye@gmail.com>
2025-03-18 12:31:53 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    
+    // Ensure that the event listener is correctly bound
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  }
+  
